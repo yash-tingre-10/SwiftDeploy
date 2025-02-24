2867aea520bbea511f4ee8b3d379cbdf5079aa91
Yash Tingre <yashtingre666@gmail.com>
2025-02-24 18:08:43 +0530

    Updated for vercel

--- Reverse Proxy Server/index.js
+++ Reverse Proxy Server/index.js
@@ -1,32 +1,24 @@
-const express = require('express')
-const httpProxy = require('http-proxy')
-require('dotenv').config();
+const express = require("express");
+const httpProxy = require("http-proxy");
+require("dotenv").config();
 
-
-const app = express()
-const PORT = process.env.REVERSE_PROXY_PORT
+const app = express();
+const proxy = httpProxy.createProxy();
 
 const BASE_PATH = process.env.AWS_BUCKET_BASE_PATH;
 
-const proxy = httpProxy.createProxy()
-
 app.use((req, res) => {
-    const hostname = req.hostname;
-    const subdomain = hostname.split('.')[0];
+  const hostname = req.hostname;
+  const subdomain = hostname.split(".")[0];
+  const resolvesTo = `${BASE_PATH}/${subdomain}`;
 
-    // Custom Domain - DB Query
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
 
-    const resolvesTo = `${BASE_PATH}/${subdomain}`
+proxy.on("proxyReq", (proxyReq, req, res) => {
+  const url = req.url;
+  if (url === "/") proxyReq.path += "index.html";
+});
 
-    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true })
-
-})
-
-proxy.on('proxyReq', (proxyReq, req, res) => {
-    const url = req.url;
-    if (url === '/')
-        proxyReq.path += 'index.html'
-
-})
-
-app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`))
+// ✅ Export for Vercel
+module.exports = app;

1f1cb25c021351e1ed76660a39dc46e527b19dcb
Yash Tingre <yashtingre666@gmail.com>
2025-02-25 06:09:45 +0530

    Updated reverse proxy

--- proxy_server/index.js
+++ proxy_server/index.js
@@ -1,25 +1,29 @@
-const express = require("express");
-const httpProxy = require("http-proxy");
-require("dotenv").config();
+const express = require('express');
+const httpProxy = require('http-proxy');
+require('dotenv').config();
 
 const app = express();
-const proxy = httpProxy.createProxy();
+const PORT = process.env.REVERSE_PROXY_PORT ;
 
 const BASE_PATH = process.env.AWS_BUCKET_BASE_PATH;
-const PORT = process.env.PORT || 3000; // Use PORT from env or default to 3000
+
+const proxy = httpProxy.createProxy();
 
 app.use((req, res) => {
   const hostname = req.hostname;
-  const subdomain = hostname.split(".")[0];
-  const resolvesTo = `${BASE_PATH}/${subdomain}`;
+  const subdomain = hostname.split('.')[0];
+
+  // Custom Domain - DB Query
+  const resolvesTo = `${BASE_PATH}${subdomain}`;
 
   return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
 });
 
-proxy.on("proxyReq", (proxyReq, req, res) => {
+proxy.on('proxyReq', (proxyReq, req, res) => {
   const url = req.url;
-  if (url === "/") proxyReq.path += "index.html";
+  if (url === '/') {
+    proxyReq.path += 'index.html';
+  }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+app.listen(PORT, () => console.log(`Reverse Proxy Running on ${PORT}`));

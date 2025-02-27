89153af102ec8f8b1b85790f9b4f8d1ba19853f4
Yash Tingre <yashtingre666@gmail.com>
2025-02-27 14:02:36 +0530

    Updated path in proxy server

--- proxy_server/index.js
+++ proxy_server/index.js
@@ -11,13 +11,14 @@ let storedProject = null;
 
 // Stream files from S3
 app.get("*", async (req, res) => {
+  const filePath = req.path.replace(/^\/+/, "");
   const deploy = req.query.deploy;
   if( deploy ) {
     storedProject = deploy;
   }
   console.log(storedProject);
   const BASE_URL = `https://swift-deploy-bucket.s3.ap-south-1.amazonaws.com/__outputs/${storedProject}`;
-  const url = BASE_URL;
+  const url = `${BASE_URL}/${filePath}`;
 
   try {
     const response = await axios({ url, responseType: "stream" });
